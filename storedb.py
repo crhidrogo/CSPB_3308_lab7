3e04dc44442ada00b20ca1191b34d1f861aa2c89
crhidrogo <claudiahidrogo+github@gmail.com>
2021-10-10 18:44:36 -0700

    added table columns
    TODO:
    - create foreign key constraints for Store_Product
    
    References:
    - [Declare Foreign Key Example](https://www.techonthenet.com/sqlite/foreign_keys/foreign_keys.php)
    - [Basic Foreign Key guide]( https://www.sqlitetutorial.net/sqlite-foreign-key/)

--- storedb.py
+++ storedb.py
@@ -2,12 +2,35 @@
 # The function should define a function create that takes a single argument (sqlite3 database filename)
 # You will need four CREATE TABLE statements in your script.
 # You can use rowid as the primary key for each of the four tables.
+import sqlite3
 
-'''
 def create(dbname):
     conn = sqlite3.connect(dbname)
-    ...
-'''
+    cur = conn.cursor()    
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Store(
+        rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+         idStore INTEGER NOT NULL
+         );
+    
+    CREATE TABLE IF NOT EXISTS Product(
+        rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+        idStore INTEGER NOT NULL
+        );
+    
+    CREATE TABLE IF NOT EXISTS Category(
+        rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+        idStore INTEGER NOT NULL
+        );
+    
+    CREATE TABLE IF NOT EXISTS Store_Product(
+        ProductID INTEGER NOT NULL,
+        StoreID INTEGER NOT NULL,
+        quantity INTEGER
+        );
+
+    ''')
 
 
 
