b3b686fb01762da35a525721abc8cff1e02b27da
crhidrogo <claudiahidrogo+github@gmail.com>
2021-10-10 17:48:27 -0700

    file set up

---
+++ storedb.py
@@ -0,0 +1,20 @@
+# Create a python file storedb.py that initializes a database
+# The function should define a function create that takes a single argument (sqlite3 database filename)
+# You will need four CREATE TABLE statements in your script.
+# You can use rowid as the primary key for each of the four tables.
+
+'''
+def create(dbname):
+    conn = sqlite3.connect(dbname)
+    ...
+'''
+
+
+
+# Create a second function fill that uses inserts to add values to each of the four tables.
+# You should have at least two rows in each table.
+'''
+def fill(dbname):
+    conn = sqlite3.connect(dbname)
+    ...
+'''
